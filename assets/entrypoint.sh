ddf33110b139d0a2296de88f9f3ab48cf5e442af
Frédéric Yhuel <frederic.yhuel@dalibo.com>
2020-09-04 12:29:07 +0200

    Add iptables rules

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -3,6 +3,12 @@
 set -e
 source /assets/colorecho
 
+ACCEPT_CIDR="192.168.0.0/24"
+iptables -A INPUT -s $ACCEPT_CIDR -j ACCEPT
+iptables -A INPUT -j DROP
+iptables -A OUTPUT -d $ACCEPT_CIDR -j ACCEPT
+iptables -A OUTPUT -j DROP
+
 if [ ! -d "/opt/oracle/app/product/11.2.0/dbhome_1" ]; then
 	echo_yellow "Database is not installed. Installing..."
 	/assets/install.sh
