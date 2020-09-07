9c207bb0faf6e9001bf40f1c93583d711f52eed7
Frédéric Yhuel <frederic.yhuel@dalibo.com>
2020-09-07 14:47:24 +0200

    remove chmod on dpdump dir

--- assets/entrypoint_oracle.sh
+++ assets/entrypoint_oracle.sh
@@ -87,7 +87,7 @@ change_dpdump_dir () {
 	while read line; do echo -e "sqlplus: $line"; done
 }
 
-chmod 777 /opt/oracle/dpdump
+#chmod 777 /opt/oracle/dpdump
 
 echo "Checking shared memory..."
 df -h | grep "Mounted on" && df -h | egrep --color "^.*/dev/shm" || echo "Shared memory is not mounted."
